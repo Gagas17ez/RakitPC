8b21e8648bbf72c1f91ab91060d4f98a3e6acbfe
Diana Kusuma <diaanakusuma@gmail.com>
2022-05-28 01:02:29 +0700

    mindah run e di main soale wedi diseneni gagas, sorry gas lali ganti wkwk

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class rakitpc extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => Rekomendasi_pc(),
+        '/': (context) => homepage(),
         '/build': (context) => pertanyaan_awal(),
         '/build/advanced': (context) => buildpc_advanced(),
         '/build/begginer/pertanyaan1': (context) => Pertanyaan1(),
